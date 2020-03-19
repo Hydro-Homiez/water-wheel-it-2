1fd2c9aac39b4e6110885016a3c87e4ecc3fff27
gracieorozco <42789089+gracieorozco@users.noreply.github.com>
2020-03-19 15:03:31 -0700

    Created clock-in system and updated templates to use components

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+import datetime
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
+# Models
 class User(db.Model):
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     first_name = db.Column(db.String(200), nullable=False)
     last_name = db.Column(db.String(200), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    in_work = db.Column(db.Boolean, nullable=False)
+
 
     def __repr__(self):
-        return '<User: %r, %r>' % self.id % self.username
+        return '<User: %r, %r, %r, %r, %r, %r, %r' % self.id % self.username % self.email % self.first_name % \
+               self.last_name % self.password % self.in_work
 
 
 class Product(db.Model):
@@ -28,6 +34,22 @@ class Product(db.Model):
         return '<Product: %r, %r, %r, %r>' % self.id % self.name % self.manufacturer % self.quantity
 
 
+class WorkTime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    employee_first_name = db.Column(db.Integer, db.ForeignKey('user.first_name'), nullable=False)
+    employee_last_name = db.Column(db.Integer, db.ForeignKey('user.last_name'), nullable=False)
+    current_time = db.Column(db.DateTime, nullable=False)
+    in_work = db.Column(db.Boolean, db.ForeignKey('user.in_work'), nullable=False)
+
+
+# class EndTime(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+#     employee_first_name = db.Column(db.Integer, db.ForeignKey('user.first_name'), nullable=False)
+#     employee_last_name = db.Column(db.Integer, db.ForeignKey('user.last_name'), nullable=False)
+#     end_time = db.Column(db.DateTime, nullable=False)
+
+
+# Page Routing
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -69,14 +91,15 @@ def new_user():
         new_first_name = request.form['first-name-input']
         new_last_name = request.form['last-name-input']
         new_password = request.form['password-input']
-        new_user = User(id=new_id, username=new_username, email=new_email, first_name=new_first_name, last_name=new_last_name, password=new_password)
+        new_user_obj = User(id=new_id, username=new_username, email=new_email, first_name=new_first_name,
+                            last_name=new_last_name, password=new_password, in_work=False)
 
         try:
-            db.session.add(new_user)
+            db.session.add(new_user_obj)
             db.session.commit()
             return redirect('/profile')
         except:
-            print(new_user.first_name)
+            print(new_user_obj.first_name)
             users = User.query.order_by(User.id).all()
             print(users)
             return 'There was an issue adding the new user: '
@@ -156,6 +179,36 @@ def profile():
 @app.route('/overview')
 def overview():
     return render_template('overview.html')
+
+
+@app.route('/time', methods=['POST', 'GET'])
+def time():
+    if request.method == 'POST':
+        t = datetime.datetime.now().replace(microsecond=0)
+        user_id = request.form['id-input']
+
+        user = User.query.get_or_404(user_id)
+        if user.in_work:
+            print("True")
+            user.in_work = False
+            new_time = WorkTime(employee_first_name=user.first_name, employee_last_name=user.last_name, current_time=t,
+                                in_work=user.in_work)
+            db.session.add(new_time)
+        else:
+            print("False")
+            user.in_work = True
+            new_time = WorkTime(employee_first_name=user.first_name, employee_last_name=user.last_name, current_time=t,
+                                in_work=user.in_work)
+            db.session.add(new_time)
+            print(f'Time: {new_time.current_time}')
+        print(f'User: {user.in_work}')
+        try:
+            db.session.commit()
+            print("Commited")
+        except:
+            print("Not commited")
+    work_times = WorkTime.query.order_by(WorkTime.current_time).all()
+    return render_template('time/time.html', work_times=work_times)
 
 
 if __name__ == "__main__":
