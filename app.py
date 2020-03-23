32765fce9a9cd4a3382b74fe14408a933b75d2bb
gracieorozco <42789089+gracieorozco@users.noreply.github.com>
2020-03-23 06:03:25 -0700

    Added search function and reusable componentts

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,16 +8,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
+# Models
 class User(db.Model):
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     first_name = db.Column(db.String(200), nullable=False)
     last_name = db.Column(db.String(200), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    in_work = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
-        return '<User: %r, %r>' % self.id % self.username
+        return '<User: %r, %r, %r, %r, %r, %r, %r' % self.id % self.username % self.email % self.first_name % \
+               self.last_name % self.password % self.in_work
 
 
 class Product(db.Model):
@@ -28,11 +33,57 @@ class Product(db.Model):
         return '<Product: %r, %r, %r, %r>' % self.id % self.name % self.manufacturer % self.quantity
 
 
+class WorkTime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    employee_first_name = db.Column(db.Integer, db.ForeignKey('user.first_name'), nullable=False)
+    employee_last_name = db.Column(db.Integer, db.ForeignKey('user.last_name'), nullable=False)
+    current_time = db.Column(db.DateTime, nullable=False)
+    in_work = db.Column(db.Boolean, db.ForeignKey('user.in_work'), nullable=False)
+
+
+# Page Routing
+# Single Page Navigation
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/profile', )
+def profile():
+    users = User.query.order_by(User.id).all()
+    return render_template('/user_management/profile.html', user_list=users)
+
+
+@app.route('/time', methods=['POST', 'GET'])
+def time():
+    if request.method == 'POST':
+        t = datetime.datetime.now().replace(microsecond=0)
+        user_id = request.form['id-input']
+
+        user = User.query.get_or_404(user_id)
+        if user.in_work:
+            user.in_work = False
+        else:
+            user.in_work = True
+        new_time = WorkTime(employee_first_name=user.first_name, employee_last_name=user.last_name, employee_id=user.id,
+                            current_time=t, in_work=user.in_work)
+        db.session.add(new_time)
+        try:
+            db.session.commit()
+        except:
+            return render_template('reusable_components/error.html', page='Clock-in',
+                                   error_message='Time not committed')
+    work_times = WorkTime.query.order_by(WorkTime.current_time).all()
+    return render_template('time/time.html', work_times=work_times)
+
+
+# User Management Navigation
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -40,20 +91,17 @@ def login():
         new_password = request.form['password-input']
 
         try:
-            name_found = False
-            password_found = False
             users = User.query.order_by(User.id).all()
             for user in users:
                 if new_username == user.username:
-                    name_found = True
                     if new_password == user.password:
-                        password_found_found = True
                         return redirect('/profile')
                     else:
-                        return 'Wrong password'
+                        return render_template('reusable_components/error.html', page='Login',
+                                               error_message='Wrong Password')
                 else:
-                    return 'User not exist'
-
+                    return render_template('reusable_components/error.html', page='Login',
+                                           error_message='User does not exist')
         except:
             return 'Error'
 
@@ -76,19 +124,23 @@ def new_user():
             db.session.commit()
             return redirect('/profile')
         except:
-            print(new_user.first_name)
-            users = User.query.order_by(User.id).all()
-            print(users)
-            return 'There was an issue adding the new user: '
+            return render_template('reusable_components/error.html', page='Insertion',
+                                   error_message='There was an issue adding the new user ')
 
     return render_template('/user_management/new_user.html')
 
 
-@app.route('/forgot_password')
-def forgot_password():
-    return render_template('/user_management/forgot_password.html')
+@app.route('/login_success')
+def login_success():
+    return render_template('/user_management/login_success.html')
 
 
+@app.route('/login_fail')
+def login_fail():
+    return render_template('/user_management/login_fail.html')
+
+
+# Product Management Navigation
 @app.route('/manage', methods=['POST', 'GET'])
 def manage():
     if request.method == 'POST':
@@ -103,7 +155,8 @@ def manage():
             db.session.commit()
             return redirect('/manage')
         except:
-            return 'There was an issue adding your product'
+            return render_template('reusable_components/error.html', page='Insertion',
+                                   error_message='There was an issue adding your product')
 
     else:
         products = Product.query.order_by(Product.id).all()
@@ -119,43 +172,42 @@ def delete(id):
         db.session.commit()
         return redirect('/manage')
     except:
-        return 'There was a problem deleting that product'
+        return render_template('reusable_components/error.html', page='Deletion',
+                               error_message='There was a problem deleting that product')
 
 
 @app.route('/manage/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     product = Product.query.get_or_404(id)
+    print(f'product {product.name}')
 
     if request.method == 'POST':
-        product.id = request.form['id-input']
+        print(f'test1')
         product.name = request.form['name-input']
         product.manufacturer = request.form['manufacturer-input']
         product.quantity = request.form['quantity-input']
+        print(f'test2')
 
         try:
             db.session.commit()
             return redirect('/manage')
         except:
-            return 'There was an issue updating your task'
+            return render_template('reusable_components/error.html', page='Update',
+                                   error_message='There was an issue updating your task')
 
     else:
         return render_template('product_management/update.html', product=product)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/profile', )
-def profile():
-    users = User.query.order_by(User.id).all()
-    return render_template('/user_management/profile.html', user_list=users)
-
-
-@app.route('/overview')
-def overview():
-    return render_template('overview.html')
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_query = request.form['search-input']
+        if search_query == "":
+            return render_template('reusable_components/error.html', page='Search',
+                                   error_message='Make sure to fill in the search bar.')
+        search_results = Product.query.filter(Product.name.contains(search_query)).all()
+    return render_template('product_management/search_results_table.html', search_results=search_results)
 
 
 if __name__ == "__main__":
